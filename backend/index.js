76fb3a75e81bbb1c3b12c1c04bbbb7aacfbe0994
Shao Yang Yu <yushaoyang@Shaos-MacBook-Air.local>
2025-05-31 15:33:38 +0800

    security fixes

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const FormDataModel = require('./models/Formdata');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 mongoose.connect(
-    'mongodb+srv://shaoyang0007:8Sz6nTTqYsCGLFtV@orb.ide4eus.mongodb.net/orbital?retryWrites=true&w=majority',
+    project.env.MONGO_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
 )
 .then(() => console.log("MongoDB connected"))
